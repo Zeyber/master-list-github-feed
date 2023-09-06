5fbc3cc5c4df30fcc69e0dfdbcfd83288acc91e4
Caine Rotherham <rotherham.c@gmail.com>
2023-09-06 08:48:47 +0200

    fix: Use root url for endpoint, update docs, update to v0.2.2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('github')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
     this.appService.initialize();
